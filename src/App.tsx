cfc20b41869539ec5b5d774f997e20348f55cd55
CreedTrue <34081464+CreedTrue@users.noreply.github.com>
2024-03-08 16:52:47 -0700

    Initial Commit
    First and maybe last commit
    - Contains all 3 required functions/components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./CollegeBasketballTeams.json";
+
+// this is the interface for the team props
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+// #1 this function welcomes the user to the page
+function PageName() {
+  return (
+    <div>
+      <h1>We Proudly Present!</h1>
+      <h2>The Marvelous Teams of March Madness!</h2>
+    </div>
+  );
+}
+
+// #2 this component creates a card for each team
+class TeamCard extends React.Component<TeamProps> {
+  render() {
+    return (
+      <div className="card">
+        <img
+          src={process.env.PUBLIC_URL + "/marchmadness.jpg"}
+          className="card-img-top"
+          alt="March Madness logo"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{this.props.school}</h5>
+          <p className="card-text">Mascot: {this.props.name}</p>
+          <p className="card-text">
+            Location: {this.props.city}, {this.props.state}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// this function creates a list of all the teams
+function TeamList() {
+  return (
+    <div>
+      <div className="row">
+        {data.teams.map((team) => (
+          <div className="col-md-4">
+            <TeamCard {...team} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: "0 auto", maxWidth: "1000px" }}>
+      <PageName />
+      <TeamList />
     </div>
   );
 }
